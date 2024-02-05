2594990fc9223ce8db9779f33f961619cbf7ee53
정구홍 <chrtfro98@gmail.com>
2024-02-05 15:16:57 +0900

    빔 활성화 수정

--- Assets/Script/SunAttack.cs
+++ Assets/Script/SunAttack.cs
@@ -1,24 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class SunAttack : MonoBehaviour
 {
+    bool isbeam = false;
     void Awake()
     {
-        
+        GetComponent<SpriteRenderer>().enabled=false;
+        GetComponent<BoxCollider2D>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle , Vector3.forward);
         transform.rotation = rotation;
 
+        if(Input.GetMouseButton(0))
+        {
+            isbeam = true;
+            GetComponent<SpriteRenderer>().enabled = isbeam;
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+        else
+        {
+            isbeam = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "wind" && isbeam == true)
+        {
+            other.gameObject.SetActive(false);
+        }
         
     }
 }
