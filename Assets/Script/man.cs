5cb25d7c51cfb7d9152a0fbf0bb04de8242f746f
Box5789 <pck279049@gmail.com>
2024-02-05 17:24:22 +0900

    나그네 옷 로직 추가

--- Assets/Script/man.cs
+++ Assets/Script/man.cs
@@ -7,28 +7,28 @@ public class man : MonoBehaviour
 {
     public int Hp;
 
-    public List<Sprite> ManSprites;
+    public List<GameObject> ManSprites;
     // Start is called before the first frame update
 
     private void Update()
     {
-        if (Hp > 75)
+        if (Hp<90&&Hp > 75)
         {
-            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[0];
+            //ManSprites[].SetActive(false);
 
         }else if (Hp > 50)
         {
-            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[1];
+            ManSprites[0].SetActive(false);
         }else if (Hp > 25)
         {
-            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[2];
+            ManSprites[1].SetActive(false);
         }else if (Hp > 0)
         {
-            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[3];
+            ManSprites[2].SetActive(false);
         }
         else
         {
-            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[4];
+            ManSprites[3].SetActive(false);
         }
         
     }
