0ea98af20db386cb899e66df00b3095c9f9281c2
Box5789 <pck279049@gmail.com>
2024-02-05 17:14:32 +0900

    나그네 이미지 추가

--- Assets/Script/man.cs
+++ Assets/Script/man.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,30 @@ using UnityEngine;
 public class man : MonoBehaviour
 {
     public int Hp;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public List<Sprite> ManSprites;
+    // Start is called before the first frame update
+
+    private void Update()
     {
+        if (Hp > 75)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[0];
+
+        }else if (Hp > 50)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[1];
+        }else if (Hp > 25)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[2];
+        }else if (Hp > 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[3];
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = ManSprites[4];
+        }
         
     }
 }
