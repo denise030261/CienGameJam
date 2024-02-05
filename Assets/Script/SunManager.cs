cc81ae2fbdf2438c361adcef7618fad551681671
정구홍 <chrtfro98@gmail.com>
2024-02-05 17:55:36 +0900

    스페이스로 태양이 나그네옷벗기기

--- Assets/Script/SunManager.cs
+++ Assets/Script/SunManager.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class SunManager : MonoBehaviour
 {
+    private man Man;
 
+    private void Start()
+    {
+        Man = GameObject.Find("Man").GetComponent<man>();
+
+    }
+
+   
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            Man.Hp-=5;
         }
 
     }
