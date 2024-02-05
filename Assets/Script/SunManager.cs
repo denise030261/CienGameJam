8522abe6f5b52e92285590bfdd1f28bc2921e852
정구홍 <chrtfro98@gmail.com>
2024-02-05 17:25:14 +0900

    임시

--- Assets/Script/SunManager.cs
+++ Assets/Script/SunManager.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 
 public class SunManager : MonoBehaviour
 {
-    public int temperature;
-    public int maxTemp;
-
-    private void Awake()
-    {
-        temperature = 0;
-        maxTemp = 100;
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            temperature++;
+
         }
 
-        if (Input.GetButtonDown("Fire1"))
-        {
-            
-        }
     }
 }
