229f7a9e3dff8932f4aa37cef1f3f8c9911c3277
Box5789 <pck279049@gmail.com>
2024-02-05 16:32:14 +0900

    제한 시간 추가

--- Assets/Script/Backgrounds.cs
+++ Assets/Script/Backgrounds.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Backgrounds : MonoBehaviour
 {
+    private bool _move = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.Instance.OnTimeEnded.AddListener(()=>
+        {
+            _move = false;
+        });
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position+=Vector3.left*Time.deltaTime;
+        if(_move)
+            transform.position+=Vector3.left*Time.deltaTime;
     }
 }
